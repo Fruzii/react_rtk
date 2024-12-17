5589f8a3ea0288fe0a9e49760a605f740da522dd
Bogdan Demchuk <bohdan1109@gmail.com>
2024-12-17 20:43:52 +0200

    add forms components

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import { Layout } from '../../components/layout'
+import React from "react";
+import { Layout } from "../../components/layout";
+import { Card, Form, Row, Space, Typography } from "antd";
+import { CustomInput } from "../../components/custom_input";
+import { PasswordInput } from "../../components/password_input";
+import { CustomButton } from "../../components/custom_button";
+import { Link } from "react-router-dom";
+import { Paths } from "../../paths";
 
 export const Login = () => {
   return (
     <Layout>
-      <div>Login</div>
+      <Row align="middle" justify="center">
+        <Card title="Log in" style={{ width: "30rem" }}>
+          <Form onFinish={() => {}}>
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Password" />
+            <CustomButton type="primary" htmlType="submit">
+              Enter
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Don't have an account? <Link to={Paths.register}>Sign up!</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
     </Layout>
-  )
-}
+  );
+};
--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
-
+import React from "react";
+import { Layout } from "../../components/layout";
+import { Card, Form, Row, Space, Typography } from "antd";
+import { CustomInput } from "../../components/custom_input";
+import { PasswordInput } from "../../components/password_input";
+import { CustomButton } from "../../components/custom_button";
+import { Link } from "react-router-dom";
+import { Paths } from "../../paths";
 export const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <Layout>
+      <Row align="middle" justify="center">
+        <Card title="Registration" style={{ width: "30rem" }}>
+          <Form onFinish={() => {}}>
+            <CustomInput name="name" placeholder="Name" />
+            <CustomInput type="email" name="email" placeholder="Email" />
+
+            <PasswordInput name="password" placeholder="Password" />
+            <PasswordInput
+              name="confirmPassword"
+              placeholder="Confirm password"
+            />
+            <CustomButton type="primary" htmlType="submit">
+              Sign up
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Already have an account? <Link to={Paths.login}>Log in!</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
+    </Layout>
+  );
+};
